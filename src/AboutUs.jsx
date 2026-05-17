eb3e0601509285b3239a882c7ad93e6446c4a0d4
jothiajith25-hue <jothiajith25@gmail.com>
2026-05-17 14:36:07 +0530

    Update and rename CONTRIBUTING.md to src/AboutUs.jsx

---
+++ src/AboutUs.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is your one-stop destination for beautiful and healthy houseplants.
+        We offer a wide variety of plants to bring freshness and greenery into your home.
+      </p>
+      <p>
+        Our mission is to make plant care simple and enjoyable for everyone.
+        Whether you are a beginner or an expert, we have something for you.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
